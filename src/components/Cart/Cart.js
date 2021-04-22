5b5b6dbe6159084bf7556fcc06fb5517fa47b88b
mifta10 <miftauddinkhan10@gmail.com>
2021-04-23 01:27:34 +0600

    shopping-cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+  const cart = props.cart;
+  const total = cart.reduce((total , prd) => total+prd.price, 0)
+  let shipping = 0;
+  if(total > 35){
+    shipping =0;
+  }
+  else if (total > 0){
+    shipping = 12.99;
+  }
+  else if (total >15){
+    shipping = 4.99;
+  }
+
+  const tax = (total/10);
+  const tb = total+ shipping;
+  const grandTotal = total + shipping + tax;
+
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision)
+  }
+
+  return (
+    <div className="container">
+        <h3>Order Summary</h3>
+        <p className="items">Items Ordered:{cart.length}</p>
+        <p><small>Items:${formatNumber(total)}</small></p>
+        <p><small>Shipping & Handling: ${formatNumber(shipping)}</small></p>
+        <p><small>Total Before Tax: ${formatNumber(tb)}</small></p>
+        <p><small>Tax: ${formatNumber(tax)}</small></p>
+        <h4 className="order-total">Order Total: ${formatNumber(grandTotal)}</h4>
+    </div>
+  );
+};
+
+export default Cart;
